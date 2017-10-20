0dd3f70eb0fa5ea5f73f3d880b7e7203f9e6ad57
FIC <f.freller@gmail.com>
2017-10-20 18:57:36 -0200

    cadastrocliente concluido

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ namespace s2d2t1
             string nome = Console.ReadLine();
             Console.WriteLine("Qual é seu e-mail?");
             string email = Console.ReadLine();
-            Console.WriteLine("Pessoa físia (digite 1), pessoa jurídica (digite 2");
+            Console.WriteLine("Pessoa físia (digite 1), pessoa jurídica (digite 2)");
             string opcao2 = Console.ReadLine();
                 switch(opcao2)
                 {
@@ -50,14 +50,14 @@ namespace s2d2t1
                         bool cpfvalido = false; 
                         Console.WriteLine("Qual é seu CPF?");
                         string cpf = Console.ReadLine();
-                        cpfvalido = checagemcpf(cpf);
+                        cpfvalido = checagemcpf(cpf,nome,email);
                         
                         break;
                     case "2":
                         bool cnpjvalido = false; 
                         Console.WriteLine("Qual é seu CNPJ?");
                         string cnpj = Console.ReadLine(); 
-                        cnpjvalido = checagemcnpj(cnpj);
+                        cnpjvalido = checagemcnpj(cnpj,nome,email);
 
                         break;
                     default: 
@@ -66,7 +66,7 @@ namespace s2d2t1
                 }
             }
         
-    static bool checagemcpf(string cpf)
+    static bool checagemcpf(string cpf, string nome, string email)
         {   bool cpfvalido = false;
             if(cpf.Length != 11)
             {
@@ -124,6 +124,10 @@ namespace s2d2t1
                 if(digito==cpfult2.ToString())
                 {
                     Console.WriteLine("CPF Válido");
+                    Console.WriteLine("Cadastro concluído com sucesso");
+                    StreamWriter cadastro = new StreamWriter ("Cadastro.txt", true);
+                    cadastro.WriteLine(nome + ";" + email + ";" + cpf + ";");
+                    cadastro.Close();
                     cpfvalido = true;
                 }
                  else
@@ -134,7 +138,7 @@ namespace s2d2t1
             }
         
         }
-    static bool checagemcnpj(string cnpj)
+    static bool checagemcnpj(string cnpj, string nome, string email)
         {bool cnpjvalido = false;
             
             if(cnpj.Length != 14)
@@ -149,9 +153,9 @@ namespace s2d2t1
                 string cnpj12, cnpj13, cnpjult2, digito, digito1, digito2;
                 int soma1=0, resto1=0, soma2=0, resto2=0;
                 cnpj12 = cnpj.Substring(0,12);
-                cnpjult2 = cnpj12.Substring(10,2);
+                cnpjult2 = cnpj.Substring(12,2);
 
-                for(int i = 0; i < 9; i++)
+                for(int i = 0; i < 12; i++)
                 {
                     soma1 += Convert.ToInt16(cnpj12[i].ToString()) * multiplicador1[i];                     
                 }
@@ -171,9 +175,9 @@ namespace s2d2t1
                 cnpj13 = cnpj12 + digito1;
                               
                 
-                for(int i=0;i<10;i++)
+                for(int i=0;i<13;i++)
                 {
-                    soma2 += Convert.ToInt32(cnpj13[i].ToString()) * multiplicador2[i]; 
+                    soma2 += Convert.ToInt16(cnpj13[i].ToString()) * multiplicador2[i]; 
                 }
                 
                 resto2 = soma2%11;
@@ -193,11 +197,15 @@ namespace s2d2t1
                 if(digito==cnpjult2.ToString())
                 {
                     Console.WriteLine("CNPJ Válido");
+                    StreamWriter cadastro = new StreamWriter ("Cadastro.txt", true);
+                    cadastro.WriteLine(nome + ";" + email + ";" + cnpj + ";");
+                    cadastro.Close();
                     cnpjvalido = true;
                 }
                  else
                 {
                      Console.WriteLine("CNPJ Inválido");
+                     Console.WriteLine(digito + cnpjult2);
                 }
                 return cnpjvalido;
             }
