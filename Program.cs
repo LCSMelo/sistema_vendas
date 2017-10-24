1733709b92a25aecba3a0992702cf70ff848e99c
Fabio Freller <f.freller@gmail.com>
2017-10-24 19:29:38 -0200

    documentado

--- Program.cs
+++ Program.cs
@@ -7,53 +7,51 @@ namespace Programar
     {static void Main(string[] args)
         {string opcao1 = "";
 
-            do
-            {
-                Console.WriteLine("Digite a opção");
-                Console.WriteLine("1 - Cadastrar Cliente");
-                Console.WriteLine("2 - Cadastrar Produto");
-                Console.WriteLine("3 - Realizar Venda");
-                Console.WriteLine("4 - Extrato Cliente");
-                Console.WriteLine("9 - Sair");
-
-                opcao1 = Console.ReadLine();
+        do
             
-                switch(opcao1)
-                {
-                    case "1": CadastrarCliente();
-                    break;
-                    case "2": CadastrarProduto();
-                    break;
-                    case "3": RealizarVenda();
-                    break;
-                    case "4": ExtratoCliente();
-                    break;
-                    case "9":
-                                {Console.WriteLine("Deseja realmente sair(s ou n)");
-                                string sair = Console.ReadLine();
-                                if(sair.ToLower().Contains("s"))
-                                    Environment.Exit(0);
-                                else if(!sair.ToLower().Contains("n"))
-                                
-                                    {opcao1 = "0";
-                                    Console.WriteLine("Opção Inválida");
-                                    }
-                                else
-                                    {opcao1 = "0";
-                                    }
+            {Console.WriteLine("Digite a opção");
+            Console.WriteLine("1 - Cadastrar Cliente");
+            Console.WriteLine("2 - Cadastrar Produto");
+            Console.WriteLine("3 - Realizar Venda");
+            Console.WriteLine("4 - Extrato Cliente");
+            Console.WriteLine("9 - Sair");
+            opcao1 = Console.ReadLine();
+            
+            switch(opcao1)
+            
+                {case "1": CadastrarCliente();
+                break;
+                case "2": CadastrarProduto();
+                break;
+                case "3": RealizarVenda();
+                break;
+                case "4": ExtratoCliente();
+                break;
+                case "9":
+                        {Console.WriteLine("Deseja realmente sair(s ou n)");
+                        string sair = Console.ReadLine();
+                        if(sair.ToLower().Contains("s"))
+                            Environment.Exit(0);
+                        else if(!sair.ToLower().Contains("n"))    
+                            {opcao1 = "0";
+                            Console.WriteLine("Opção Inválida");
                             }
-                    
-
+                        else
+                            {opcao1 = "0";
+                            }
+                        }                 
                              
-                    break;
-                }
+                break;
             }
-   
-            while (opcao1 != "9");
         }
-        static void CadastrarCliente()
-            {
-            Console.WriteLine("Cadastro");
+        while (opcao1 != "9");
+        }
+    /// <summary>
+    /// Cadastra Cliente
+    /// </summary>
+    static void CadastrarCliente()
+            
+            {Console.WriteLine("Cadastro");
             Console.WriteLine("Qual é seu nome?");
             string nome = Console.ReadLine();
             Console.WriteLine("Qual é seu e-mail?");
@@ -61,8 +59,8 @@ namespace Programar
             Console.WriteLine("Pessoa física (digite 1), pessoa jurídica (digite 2)");
             string opcao2 = Console.ReadLine();
             switch(opcao2)
-            {
-                case "1":
+            
+                {case "1":
                         bool cpfvalido = false; 
                         Console.WriteLine("Qual é seu CPF?");
                         string cpf = Console.ReadLine();
@@ -81,18 +79,24 @@ namespace Programar
                 break;
                 }
             }
-        
+    /// <summary>
+    /// Checa validade do CPF
+    /// </summary>
+    /// <param cpf="cpf"></param>
+    /// <param nome="nome"></param>
+    /// <param email="email"></param>
+    /// <returns></returns>    
     static bool checagemcpf(string cpf, string nome, string email)
        
        {bool cpfvalido = false;
         if(cpf.Length != 11)
-        {
-            Console.WriteLine("CPF Inválido"); 
+        
+            {Console.WriteLine("CPF Inválido"); 
             return cpfvalido;         
-        }
+            }
         else
-        {
-            int[] multiplicador1 = new int[]{10,9,8,7,6,5,4,3,2};
+        
+            {int[] multiplicador1 = new int[]{10,9,8,7,6,5,4,3,2};
             int[] multiplicador2 = new int[]{11,10,9,8,7,6,5,4,3,2};
             string cpf9, cpf10, cpfult2, digito, digito1, digito2;
             int soma1=0, resto1=0, soma2=0, resto2=0;
@@ -100,65 +104,71 @@ namespace Programar
             cpfult2 = cpf.Substring(9,2);
 
             for(int i = 0; i < 9; i++)
-            {
-                soma1 += Convert.ToInt16(cpf9[i].ToString()) * multiplicador1[i];                     
-            }
-
+            
+                {soma1 += Convert.ToInt16(cpf9[i].ToString()) * multiplicador1[i];                     
+                }
             resto1 = soma1%11;
 
             if(resto1 < 2)
-            {
-                resto1 = 0;
-            }
+            
+                {resto1 = 0;
+                }
             else
-            {
-                resto1 = 11 - resto1;
-            }
+            
+                {resto1 = 11 - resto1;
+                }
                 
             digito1 = resto1.ToString();
             cpf10 = cpf9 + digito1;
                               
                 
             for(int i=0;i<10;i++)
-            {
-                soma2 += Convert.ToInt32(cpf10[i].ToString()) * multiplicador2[i]; 
-            }
+            
+                {soma2 += Convert.ToInt32(cpf10[i].ToString()) * multiplicador2[i]; 
+                }
                 
             resto2 = soma2%11;
 
             if(resto2 < 2)
-            {
-                resto2 = 0;
-            }
+            
+                {resto2 = 0;
+                }
             else
-            {
-                resto2 = 11 - resto2;
-            }
+            
+                {resto2 = 11 - resto2;
+                }
 
             digito2 = resto2.ToString();
             digito = digito1 + digito2;
 
             if(digito==cpfult2.ToString())
-            {
-                Console.WriteLine("CPF Válido");
+            
+                {Console.WriteLine("CPF Válido");
                 StreamWriter cadastro = new StreamWriter ("Cadastro.txt", true);
                 FileInfo cabecalho = new FileInfo("Cadastro.txt");
                 if(cabecalho.Length == 0)
-                {
-                    cadastro.WriteLine ("NOME; E-MAIL; CPF/CNPJ; DATA");
-                }
+                
+                    {cadastro.WriteLine ("NOME; E-MAIL; CPF/CNPJ; DATA");
+                    }
                     cadastro.WriteLine(nome + ";" + email + ";" + cpf + ";" + DateTime.Now);
                     cadastro.Close();
                     cpfvalido = true;
-            }
+                }
             else
-            {
-                 Console.WriteLine("CPF Inválido");      
-            }
+            
+                {Console.WriteLine("CPF Inválido");      
+                }
             return cpfvalido;
-        }
+            }
         
         }
+    /// <summary>
+    /// Checa CNPJ
+    /// </summary>
+    /// <param cnpj="cnpj"></param>
+    /// <param nome="nome"></param>
+    /// <param email="email"></param>
+    /// <returns></returns>
     static bool checagemcnpj(string cnpj, string nome, string email)
         {bool cnpjvalido = false;
             
@@ -237,8 +247,9 @@ namespace Programar
         
         }
     
-
-
+    /// <summary>
+    /// Cadastra Produto
+    /// </summary>
     static void CadastrarProduto()
     
         {Console.WriteLine("Qual o nome do produto?");
@@ -259,6 +270,9 @@ namespace Programar
             cadastroproduto.WriteLine ("NOME DO PRODUTO; CÓDIGO DO PRODUO; DESCRIÇÃO DO PRODUTO; PREÇO;");
         }
         }
+    /// <summary>
+    /// Realiza Venda
+    /// </summary>
     static void RealizarVenda()
     
         {Console.WriteLine("Digite seu CPF");
@@ -270,19 +284,18 @@ namespace Programar
         {
             if(linha.Contains(cpfvenda) == true)
             {
-               cpfencontrado = true;
+                cpfencontrado = true;
                 linhacliente = linha;
                 break;
             }
             else
             {
-            cpfencontrado = false;
+                cpfencontrado = false;
             }            
         }
     
         if(cpfencontrado == true)
         {
-            Console.WriteLine("CPF Válido");
             string[] produtos = File.ReadAllLines("Cadastroproduto.txt");
             foreach(string produtovenda in produtos)
             {
@@ -303,8 +316,12 @@ namespace Programar
             {
                 if(linhaproduto.Contains(codigoproduto) == true)
                 {
-                cadastrovendas.WriteLine(linhacliente + ";" + linhaproduto + ";" + DateTime.Now + ";");
-                cadastrovendas.Close();
+                    cadastrovendas.WriteLine(linhacliente + ";" + linhaproduto + ";" + DateTime.Now + ";");
+                    cadastrovendas.Close();
+                }
+                else
+                {
+                    Console.WriteLine("Código Inválido, tente novamente");
                 }
                         
             }
@@ -314,7 +331,10 @@ namespace Programar
             Console.WriteLine("CPF Inválido");
             CadastrarCliente();
         }
-        } 
+        }
+    /// <summary>
+    /// Gera extrato para cliente
+    /// </summary>     
     static void ExtratoCliente()
         
         {Console.WriteLine("Pessoa física (digite 1), pessoa jurídica (digite 2)");
